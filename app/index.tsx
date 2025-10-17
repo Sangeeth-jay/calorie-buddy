0f9b4393f2d3aa8c81c7bb2ef42375c69b400c93
Sangeeth-jay <sangeethjayawarna@gmail.com>
2025-10-17 09:33:02 +0530

    change splash screen

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { Image, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex w-full h-full flex-col bg-blue-500 items-center justify-center gap-4">
+      <View className="bg-[#f4f4f4] w-36 h-36 flex items-center justify-center rounded-full">
+        <Image
+          source={require("../assets/images/logo.png")}
+          className="w-fit h-fit"
+        />
+      </View>
+      <Text className="text-white text-2xl font-semibold">Hello world</Text>
+    </View>
+  );
+}
