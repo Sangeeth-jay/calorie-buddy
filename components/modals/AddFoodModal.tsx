6122d83bdb365d4a81b53faef981def2aefc5116
Sangeeth-jay <sangeethjayawarna@gmail.com>
2026-02-10 22:58:28 +0530

    implement food search functionality in AddFoodModal

--- components/modals/AddFoodModal.tsx
+++ components/modals/AddFoodModal.tsx
@@ -1,12 +1,19 @@
 import { View, Text, Pressable } from "react-native";
-import React, { useCallback, useRef, useEffect } from "react";
+import React, { useCallback, useRef, useEffect, useState } from "react";
 import {
   BottomSheetModal,
   BottomSheetView,
   BottomSheetBackdrop,
   BottomSheetTextInput,
 } from "@gorhom/bottom-sheet";
-import {  MagnifyingGlassIcon, BarcodeIcon, CubeFocusIcon } from "phosphor-react-native";
+import {
+  MagnifyingGlassIcon,
+  BarcodeIcon,
+  CubeFocusIcon,
+  PlusIcon,
+} from "phosphor-react-native";
+import { searchFoods } from "@/src/services/foodSearch";
+import { ScrollView } from "react-native-gesture-handler";
 
 interface AddFoodModalProps {
   isOpen: boolean;
@@ -15,6 +22,11 @@ interface AddFoodModalProps {
   onSave?: (data: any) => void;
 }
 
+type FoodSearchResult = {
+  status: "idle" | "loading" | "success" | "empty" | "error";
+  data: any[];
+};
+
 const AddFoodModal: React.FC<AddFoodModalProps> = ({
   isOpen,
   onClose,
@@ -22,6 +34,11 @@ const AddFoodModal: React.FC<AddFoodModalProps> = ({
   onSave,
 }) => {
   const bottomSheetModalRef = useRef<BottomSheetModal>(null);
+
+  const [query, setQuery] = useState("");
+  const [result, setResult] = useState<any[]>([]);
+  const [status, setStatus] = useState<FoodSearchResult["status"]>("idle");
+  const debounceRef = useRef<NodeJS.Timeout | null>(null);
 
   useEffect(() => {
     if (isOpen) {
@@ -37,7 +54,7 @@ const AddFoodModal: React.FC<AddFoodModalProps> = ({
         onClose();
       }
     },
-    [onClose]
+    [onClose],
   );
 
   const renderBackdrop = useCallback(
@@ -49,8 +66,38 @@ const AddFoodModal: React.FC<AddFoodModalProps> = ({
         opacity={0.5}
       />
     ),
-    []
+    [],
   );
+
+  //searching debounce
+  useEffect(() => {
+    if (debounceRef.current) clearTimeout(debounceRef.current);
+
+    if (query.trim().length < 2) {
+      setStatus("idle");
+      setResult([]);
+      return;
+    }
+
+    debounceRef.current = setTimeout(async () => {
+      setStatus("loading");
+
+      const res = await searchFoods(query);
+
+      // console.log("STATUS:", res.status);
+      // console.log("RESULT:", res.data);
+      // console.log("RESULT LENGTH:", res.data.length);
+
+      setStatus(res.status as FoodSearchResult["status"]);
+      setResult(res.data);
+    }, 400);
+
+    return () => {
+      if (debounceRef.current) {
+        clearTimeout(debounceRef.current);
+      }
+    };
+  }, [query]);
 
   return (
     <BottomSheetModal
@@ -59,12 +106,14 @@ const AddFoodModal: React.FC<AddFoodModalProps> = ({
       backdropComponent={renderBackdrop}
       enablePanDownToClose
       snapPoints={["90%"]}
-      handleStyle={{ backgroundColor: "#3b82f6", borderTopEndRadius: 14, borderTopStartRadius: 14 }}
+      handleStyle={{
+        backgroundColor: "#3b82f6",
+        borderTopEndRadius: 14,
+        borderTopStartRadius: 14,
+      }}
       handleIndicatorStyle={{ backgroundColor: "#ffffff" }}
     >
       <BottomSheetView className="flex-1 bg-white">
-
-
         {/* Meal Type Badge */}
         <View className="bg-blue-500 py-5 items-center rounded-b-full mb-4">
           <Text className="text-white text-3xl font-semibold">{mealType}</Text>
@@ -77,38 +126,85 @@ const AddFoodModal: React.FC<AddFoodModalProps> = ({
             className="flex-1 text-base"
             placeholder="Search"
             placeholderTextColor="#9ca3af"
+            value={query}
+            onChangeText={setQuery}
           />
         </View>
 
         {/* Action Buttons */}
         <View className="flex-row gap-3 px-4 mb-4">
           <Pressable className="flex-1 bg-blue-500 rounded-xl py-5 items-center gap-2">
-            <CubeFocusIcon size={32} color="#fff"  />
+            <CubeFocusIcon size={32} color="#fff" />
             <Text className="text-white text-sm font-semibold">Scan meal</Text>
           </Pressable>
           <Pressable className="flex-1 bg-blue-500 rounded-xl py-5 items-center gap-2">
             <BarcodeIcon size={32} color="#fff" />
-            <Text className="text-white text-sm font-semibold">Scan barcode</Text>
+            <Text className="text-white text-sm font-semibold">
+              Scan barcode
+            </Text>
           </Pressable>
         </View>
 
         {/* Recently Added Foods */}
         <View className="flex-1 px-4">
-          <Text className="text-xs text-gray-400 mb-2">Recently added foods</Text>
+          <Text className="text-xs text-gray-400 mb-2">
+            Recently added foods
+          </Text>
           <View className="flex-1 justify-center items-center">
-            <Text className="text-base text-gray-300">N/A</Text>
+            {/* Loading */}
+            {status === "loading" && (
+              <Text className="text-sm text-gray-400 mt-2">
+                Searching food...
+              </Text>
+            )}
+
+            {/* Results */}
+            {status === "success" && result.length > 0 && (
+              <ScrollView
+                className="mt-2 w-full"
+                contentContainerClassName="px-4 pb-6"
+                showsVerticalScrollIndicator={false}
+                style={{ maxHeight: result.length > 4 ? 320 : undefined }}
+              >
+                {result.map((item) => (
+                  <Pressable
+                    key={item.id}
+                    className="w-full px-4 py-3 mb-3 bg-gray-100 rounded-xl active:bg-gray-200 flex-row items-center justify-between"
+                  >
+                    <View>
+                      <Text className="font-normal text-lg">{item.name}</Text>
+                      <Text className="font-light text-sm">
+                        {item.calories ?? "--"} cal,{" "}
+                        {item.portion ?? "1 serving"}
+                      </Text>
+                    </View>
+
+                    <View className="p-2 bg-blue-100 rounded-full">
+                      <PlusIcon size={28} color="#3b82f6" />
+                    </View>
+                  </Pressable>
+                ))}
+              </ScrollView>
+            )}
+
+            {/* Empty */}
+            {status === "empty" && (
+              <View className="mt-4 items-center">
+                <Text className="text-gray-400 text-sm">N/A</Text>
+              </View>
+            )}
           </View>
         </View>
 
         {/* Bottom Buttons */}
         <View className="flex-row gap-3 px-4 py-4">
-          <Pressable 
+          <Pressable
             onPress={onClose}
             className="flex-1 bg-[#e5e7eb] rounded-full py-4 items-center"
           >
             <Text className="text-lg font-semibold text-gray-700">Cancel</Text>
           </Pressable>
-          <Pressable 
+          <Pressable
             onPress={onClose}
             className="flex-1 bg-blue-500 rounded-full py-4 items-center"
           >
